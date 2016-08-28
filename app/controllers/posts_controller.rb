5a5b358cc0ad8bfbbc54f1cd9a8bab5b62964952
railsbeginner <bartosz.dyja@gmail.com>
2016-08-28 20:01:59 +0200

    Define simple CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, flash: { success: 'Post added' }
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.delete
+      redirect_to posts_path, flash: { success: 'Post deleted' }
+    end
+  end
+
   def index
     @posts = Post.all.includes(:user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  get 'posts/index'
+  resources :posts, only: [:new, :create, :index, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
